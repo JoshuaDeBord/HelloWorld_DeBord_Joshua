a649f006a7bafea518a328191957ec3c5fc372d1
JoshuaDeBord <114021926+JoshuaDeBord@users.noreply.github.com>
2023-02-22 15:06:00 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace HelloWorld_DeBord_Joshua
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            string lastName = Console.ReadLine();
+            
+            Console.WriteLine("Hello, {firstName} {lastName}!");
+            
+            Console.WriteLine("How has your day been?");
+            string dayBeen = Console.ReadLine();
+
+            Console.WriteLine($"I'm happy to know that!");
+        }
+    }
+}
