38132ec18e24e1c1848ce548cec72c3fc072f3a7
JoshuaDeBord <114021926+JoshuaDeBord@users.noreply.github.com>
2023-02-23 13:33:02 -0600

    RICK ROLL

--- Program.cs
+++ Program.cs
@@ -2,45 +2,151 @@
 
 namespace HelloWorld_DeBord_Joshua
 {
-    class Program
+    class Program    //If you see this, I'm watching you!
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi! Please answer the following questions!");
+            Console.WriteLine("Hi! My name is Jeff the fun bear! Please answer the following questions! We're watching your every step so be honest!");
             
-            Console.WriteLine("What is your first name?");
+            Console.WriteLine("So where do we start? Oh yeah.. What is your first name?");
             string firstName = Console.ReadLine();
-            Console.WriteLine("What is your last name?");
+            Console.WriteLine("Thats good to know.... Now... What is your last name?...");
             string lastName = Console.ReadLine();
-            
-            Console.WriteLine($"Hello, {firstName} {lastName}!");
-            
-            Console.WriteLine("How has your day been?");
+            Console.WriteLine("Now what's your social security?...");
+            string socialSecurity = Console.ReadLine();
+            Console.WriteLine("Now... what's your credit card number?...");
+            string cardNumber = Console.ReadLine();
+            Console.WriteLine($"Hello, {firstName} {lastName}... We have a few more questions to ask you..");
+            string questionsAsk = Console.ReadLine();
+            Console.WriteLine("How has your day been?...");
             string dayBeen = Console.ReadLine();
+            Console.WriteLine("I'm happy you told me that...");  //This is actually extremely cool! I plan to keep learning this as I'm am positive it could help me in the future.!
+            Console.WriteLine("Have you experienced anything unusual recently?...");
+            string experiencedUnsual = Console.ReadLine();
+            Console.WriteLine("Well.. I'm gonna leave you here... That's all the information I need from you.. for now. Don't forget we're always watching.");
 
-            Console.WriteLine($"I'm happy to know that!");  //This is actually extremely cool! I plan to keep learning this as I'm am positive it could help me in the future.!
+
+
 
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("We're no strangers to love");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("You know the rules and so do I (do I)");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("A full commitment's what I'm thinking of");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("You wouldn't get this from any other guy");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("I just wanna tell you how I'm feeling");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Gotta make you understand");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna give you up");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna let you down");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna run around and desert you");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna make you cry");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!"); 
+            Console.WriteLine("Never gonna say goodbye");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna tell a lie and hurt you");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("We've known each other for so long");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Your heart's been aching, but you're too shy to say it (say it)");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Inside, we both know what's been going on (going on)");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("We know the game and we're gonna play it");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("And if you ask me how I'm feeling");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Don't tell me you're too blind to see");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna give you up");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna let you down");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
-
+            Console.WriteLine("Never gonna run around and desert you");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna make you cry");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna say goodbye");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna tell a lie and hurt you");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("We've known each other for so long");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Your heart's been aching, but you're too shy to say it (say it)");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Inside, we both know what's been going on (going on)");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("We know the game and we're gonna play it");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("And if you ask me how I'm feeling");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Don't tell me you're too blind to see");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna give you up");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna let you down");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna run around and desert you");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna make you cry");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna say goodbye");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna tell a lie and hurt you");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna give you up");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna let you down");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna run around and desert you");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna make you cry");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna say goodbye");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna tell a lie and hurt you");
+            Console.WriteLine("We've known each other for so long");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Your heart's been aching, but you're too shy to say it (to say it)");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Inside, we both know what's been going on (going on)");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("We know the game and we're gonna play it");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("I just wanna tell you how I'm feeling");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Gotta make you understand");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna give you up");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna let you down");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna run around and desert you");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna make you cry");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna say goodbye");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna tell a lie and hurt you");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna give you up");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna let you down");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna run around and desert you");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna make you cry");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna say goodbye");
+            Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
+            Console.WriteLine("Never gonna tell a lie and hurt you");
             Console.WriteLine("YoU'Ve BeEn ScAmMeD!!");
         }
     }
